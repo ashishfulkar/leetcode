959482920bd2906823758b6ee662410edaf22dca
coolashx <arfulkar@gmail.com>
2024-04-09 20:11:02 +0530

    Best solution

--- src/main/java/com/ashx/leetcode/google/medium/TheEarliestMomentWhenEveryoneBecomeFriends.java
+++ src/main/java/com/ashx/leetcode/google/medium/TheEarliestMomentWhenEveryoneBecomeFriends.java
@@ -17,7 +17,7 @@ public class TheEarliestMomentWhenEveryoneBecomeFriends {
         // int n = 4;
         int[][] logs = {{5, 3, 2}, {3, 0, 1}, {2, 2, 0}, {10, 2, 4}, {0, 3, 1}, {8, 3, 4}};
         int n = 5;
-        System.out.println(f.earliestAcqBest(logs, n));
+        System.out.println(f.earliestAcqPerf(logs, n));
     }
 
     public int earliestAcq(int[][] logs, int n) {
@@ -112,6 +112,30 @@ public class TheEarliestMomentWhenEveryoneBecomeFriends {
             }
         }
         return true;
+    }
+
+    private int find(int[] par, int u) {
+        if (par[u] != u) par[u] = find(par, par[u]);
+        return par[u];
+    }
+
+    private boolean join(int[] par, int x, int y) {
+        int p1 = find(par, x), p2 = find(par, y);
+        if (p1 == p2) return false;
+        if (p1 > p2) par[p1] = p2;
+        else par[p2] = p1;
+        return true;
+    }
+
+    public int earliestAcqPerf(int[][] logs, int n) {
+        int[] par = new int[n];
+        for (int i = 0; i < n; i++) par[i] = i;
+        Arrays.sort(logs, Comparator.comparingInt(a -> a[0]));
+        for (int[] log : logs) {
+            if (join(par, log[1], log[2])) n--;
+            if (n == 1) return log[0];
+        }
+        return -1;
     }
 }
 
