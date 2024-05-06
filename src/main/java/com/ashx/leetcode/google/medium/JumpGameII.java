377fcdf8685230b85548cc873f089b668d44ae47
coolashx <arfulkar@gmail.com>
2024-05-06 22:46:40 +0530

    JumpGameII

--- src/main/java/com/ashx/leetcode/google/medium/JumpGameII.java
+++ src/main/java/com/ashx/leetcode/google/medium/JumpGameII.java
@@ -10,7 +10,7 @@ public class JumpGameII {
         // int[] nums = {2, 3, 0, 1, 4}; // 2
         // int[] nums = {2, 0, 2, 4, 6, 0, 0, 3}; // 3
         int[] nums = {5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0}; // 3
-        System.out.println(g.jump(nums));
+        System.out.println(g.jumpPerf(nums));
     }
 
     public int jump(int[] nums) {
@@ -39,5 +39,29 @@ public class JumpGameII {
         }
 
         return jumps[0];
+    }
+
+    public int jumpPerf(int[] nums) {
+        return nums.length == 1 ? 0 : jumpsToTarget(nums, 0);
+    }
+
+    public int jumpsToTarget(int[] nums, int start) {
+        int possibleJumps = nums[start];
+        int target = nums.length - 1;
+        int maxJumps = 0;
+        int nextStart = 0;
+
+        if (start == target) return 0;
+
+        if (start + possibleJumps >= target) return 1;
+
+        for (int i = 1; i <= possibleJumps; i++) {
+            if (i + nums[start + i] > maxJumps) {
+                maxJumps = i + nums[start + i];
+                nextStart = start + i;
+            }
+        }
+
+        return 1 + jumpsToTarget(nums, nextStart);
     }
 }
