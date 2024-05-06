b3d79751d6eacf74da838e86f8fbb526c930e46e
coolashx <arfulkar@gmail.com>
2024-05-06 22:43:26 +0530

    JumpGameII

---
+++ src/main/java/com/ashx/leetcode/google/medium/JumpGameII.java
@@ -0,0 +1,43 @@
+package com.ashx.leetcode.google.medium;
+
+/**
+ * <a href="https://leetcode.com/problems/jump-game-ii/">Leetcode</a>
+ */
+public class JumpGameII {
+    public static void main(String[] args) {
+        JumpGameII g = new JumpGameII();
+        // int[] nums = {2, 3, 1, 1, 4}; // 2
+        // int[] nums = {2, 3, 0, 1, 4}; // 2
+        // int[] nums = {2, 0, 2, 4, 6, 0, 0, 3}; // 3
+        int[] nums = {5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0}; // 3
+        System.out.println(g.jump(nums));
+    }
+
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] jumps = new int[n];
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            int reach = i + nums[i];
+            if (reach >= n - 1) {
+                jumps[i] = 1;
+                continue;
+            }
+            int min = Integer.MAX_VALUE;
+            while (reach > i) {
+                if (jumps[reach] > 0) {
+                    min = Math.min(min, jumps[reach]);
+                }
+                reach--;
+            }
+            if (min != Integer.MAX_VALUE) {
+                jumps[i] = min + 1;
+            }
+        }
+
+        return jumps[0];
+    }
+}
